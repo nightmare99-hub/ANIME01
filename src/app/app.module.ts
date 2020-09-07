3b4ec5f1434ccb0887dac3bd1b9b656620547daa
unknown <nightmare68995@gmail.com>
2020-09-07 13:29:30 +0700

    alo

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
+
+import {ApproutingModule} from './app-routing.module';
+import {MenuComponent} from './Components/Blocks/Menu/menu.component';
+import {SliderComponent} from './Components/Blocks/Slider/Slider.component';
+import {UsersComponent} from './Components/Blocks/Users/Users.component';
+import {CategoriesComponent} from './Components/Blocks/Categories/Categories.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SliderComponent,
+    UsersComponent,
+    CategoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    ApproutingModule,
+    
+    BsDropdownModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
